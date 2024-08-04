245e83ef39b9cf86b7ef051c074fec1a2c6ada55
AcerStructor <acerstructordev@gmail.com>
2024-08-04 11:46:41 +0800

    Modified directories

--- src/utils.c
+++
@@ -1,9 +0,0 @@
-#include "utils.h"
-
-float Get_RectMidPoint(const float centerVal, const float ScaleVal)
-{
-    const float midPointVal = (ScaleVal / 2); // calculate to get the middle
-                                              // point
-    return (centerVal - midPointVal);
-}
-
